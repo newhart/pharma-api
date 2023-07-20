1ad01b795a654a473446926035c39d59193c3700
newhart <dev.haritianakely@gmail.com>
2023-07-20 12:55:24 +0300

    feat(database/seeder/SaleSeeder.php): add seeder for all sale

---
+++ database/seeders/SaleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Sale;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SaleSeeder extends Seeder
+{
+    use WithoutModelEvents ;
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sales = Sale::all();
+        foreach ($sales as $sale){
+            if($sale->saleStay === 0.0){
+                $sale->stateSale = 'Valider';
+                $sale->user_id = 1;
+                $sale->save();
+            }
+        }
+    }
+}
